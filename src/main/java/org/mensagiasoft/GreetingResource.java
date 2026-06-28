b2a1af36e8bdf9045a369142d25b8b80b206780d
jevillac07 <jevillac07@gmail.com>
2026-06-28 15:22:54 -0500

    add base project

---
+++ src/main/java/org/mensagiasoft/GreetingResource.java
@@ -0,0 +1,17 @@
+package org.mensagiasoft;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+
+@Path("/hello")
+public class GreetingResource {
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String hello(@PathParam("name") String name) {
+        return "Hello from Quarkus REST " + name;
+    }
+}

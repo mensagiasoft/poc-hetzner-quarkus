2c75cfc9c4f2ee344a0fa6da06d52f43116bc2c4
jevillac07 <jevillac07@gmail.com>
2026-06-29 10:25:27 -0500

    fix: modify java controller and wrapper

--- src/main/java/org/mensagiasoft/HelloResource.java
+++ src/main/java/org/mensagiasoft/HelloResource.java
@@ -4,7 +4,6 @@ import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
-import jakarta.ws.rs.core.MediaType;
 
 @Path("/hello")
 public class HelloResource {
